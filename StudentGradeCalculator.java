fd8df2d6c1f9f3fbadf71cf46692621767399183
Varad Kanekar <kanekarvarad@gmail.com>
2024-07-27 19:51:27 +0530

    Initialize variables for storing total marks and number of subjects

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Student Grade Calculator!");
+
+        System.out.print("Enter the number of subjects: ");
+        int numberOfSubjects = scanner.nextInt();
+
+        int[] marks = new int[numberOfSubjects];
+        int totalMarks = 0;
+    }
+}
